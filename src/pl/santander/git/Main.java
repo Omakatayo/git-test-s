50d2a3d0277b42c7a7980bf3dd41955b6ec70abb
Omakatayo <labedz.sebastian@gmail.com>
2021-06-12 11:31:44 +0200

    fourth commit

--- src/pl/santander/git/Main.java
+++ src/pl/santander/git/Main.java
@@ -3,5 +3,6 @@ package pl.santander.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("New line of code Master");
     }
 }
